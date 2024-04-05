dd42f10db6e66e351cc4d1bcc1c52183f83c2290
privatebusiness88 <131011829+privatebusiness88@users.noreply.github.com>
2024-04-05 23:39:15 +0700

    Create check.ts

---
+++ public/check.ts
@@ -0,0 +1,29 @@
+use Illuminate\Http\Request ;
+use Illuminate\App\Database;
+
+private $token.Data;
+private $token.Connection;
+
+
+return $this.Token => $token.Data;
+Db:Save $Token.Data;
+
+Return $token.data => $token.HasFactory;
+Return $token.hasFactory => $token.Factory;
+Db:save $TOkenFactory;
+
+Return $token.Factory => $token.HasManyData;
+Db:Save $TokenManyData;
+
+
+
+trait HasFactory() {
+void};
+
+$tokenManyData == Trait.HasFactory (){
+run == void};
+
+DB:Save;
+Db:Update;
+
+DB:Seed --class Trait.HasFactory(){Run==void };
